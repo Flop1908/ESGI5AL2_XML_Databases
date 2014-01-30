a6186ded371bf68ed37a30cf2b5821939277d093
Flop1908 <moijesuisflorent@gmail.com>
2014-01-30 15:53:04 +0100

    Lionel: Ajout de la classe BaseEnXML

---
+++ XML-java/src/Model/BaseEnXML.java
@@ -0,0 +1,21 @@
+package Model;
+
+/**
+ * 
+ * @author Lionel
+ *
+ */
+public class BaseEnXML {
+
+	//Etape 1: Trouver le nom de l'element racine 
+	//Etape 2: Pour chaque table, ouvrir un element avec le nom de la table avec un "s"
+	//Etape 3: Pour chaque ligne de la table ouvrir un balise avec le nom sans le "s"
+	//Etape 4: Pour chaque ligne mettre une balise avec le nom de la colonne
+	//Etape 5: Mettre la valeur
+	//Etape 6: Fermer la balise avec le nom de la colonne
+	//Etape 7: Fermer la balise avec le nom de la table sans "s"
+	//Etape 8: Fermer la balise avec le nom de la table avec "s"
+	//Etape 9: Et fermer avec le nom de la racine 
+}
+
+	
